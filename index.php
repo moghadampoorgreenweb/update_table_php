e60e9c56c1b443e59d899ec2ab7aebe768e48903
moghadampoorgreenweb <100747335+moghadampoorgreenweb@users.noreply.github.com>
2023-08-13 10:07:44 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+      $oldTable   =  collect(json_decode(file_get_contents(__DIR__.'/exportOldTable.json'),true));
+      $targetTable =  collect(json_decode(file_get_contents(__DIR__.'/exportTargetTable.json'),true));
+      $targetTable = $targetTable->pluck('value','key');
+      $oldTable   = $oldTable->pluck('value','key');
+      $outQuery   =  $targetTable->mapWithKeys(function ($value, $resipient) use ($oldTable){
+
+          /*
+          |--------------------------------------------------------------------------
+          | condition update
+          |--------------------------------------------------------------------------
+          */
+         if (isset($oldTable[$resipient]) && !empty($oldTable[$resipient]))
+          return [$resipient => $oldTable[$resipient]];
+
+          return [$resipient => $value];
+
+        })->map(fn ($value,$resipient)=> [
+                'UPDATE `targetTable` SET `targetColumn` =',
+                '\'',
+                 "{$value}",
+                '\'',
+                " WHERE `black_lists`.`recipient` = $resipient;"
+        ])->map(fn ($resipient) => implode('',$resipient))->values()->toArray();
+
+      
+      file_put_contents(__DIR__.'/out.json',json_encode(implode(' ',$outQuery),JSON_UNESCAPED_UNICODE));
+      die;
