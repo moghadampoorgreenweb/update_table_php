9bdd6218dffe2ca5ce41a43882337e54189d2bd9
moghadampoorgreenweb <100747335+moghadampoorgreenweb@users.noreply.github.com>
2023-08-14 11:00:37 +0330

    Update index.php

--- index.php
+++ index.php
@@ -26,3 +26,56 @@
       
       file_put_contents(__DIR__.'/out.json',json_encode(implode(' ',$outQuery),JSON_UNESCAPED_UNICODE));
       die;
+
+
+
+
+/*
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+
+
+
+
+          /*
+          |--------------------------------------------------------------------------
+          | manage srting start and end
+          |--------------------------------------------------------------------------
+          */
+        function deleteLinesFromStart($string, $lineCount) {
+            $lines = explode("\n", $string);
+            $remainingLines = array_slice($lines, $lineCount);
+            $result = implode("\n", $remainingLines);
+            return $result;
+        }
+        function deleteLinesFromEnd($string, $lineCount) {
+            $lineBreaks = substr_count($string, "\n");
+            $linesToDelete = min($lineBreaks, $lineCount);
+
+            $lastLineIndex = strlen($string) - 1;
+            for ($i = 0; $i < $linesToDelete; $i++) {
+                $lastLineIndex = strrpos($string, "\n", $lastLineIndex - strlen($string) - 1);
+            }
+
+            if ($lastLineIndex !== false) {
+                $string = substr($string, 0, $lastLineIndex);
+            }
+
+            return $string;
+        }
+        
+        $tblemailtemplates = collect(json_decode(file_get_contents(__DIR__.'/tblemailtemplates.json'),true)[2]['data']);
+
+        foreach ($tblemailtemplates->pluck('message_fa') as $htm) {
+            
+            $result = deleteLinesFromStart($htm, 27);
+            $result = deleteLinesFromEnd($result, 22);
+            
+           if (preg_match("/<\/td>\s*<\/tr>\s*$/",$result)) {
+               $result = deleteLinesFromEnd($result, 2);
+           }
+           
+            $outPut[]  = $result;
+        }
+
